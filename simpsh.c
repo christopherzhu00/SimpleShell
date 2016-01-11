bfa7c75ef4a8548624229f32a9342c9a347ff39f
iamthebestzhu <chrszhu@yahoo.com>
2016-01-10 16:51:37 -0800

    verbose is added

--- simpsh.c
+++ simpsh.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 int* arguments;
 int counter = 0;
+int verboseFlag = 0;
 
 int main(int argc, char *argv[])
 {
@@ -28,6 +32,7 @@ int main(int argc, char *argv[])
 	{
 		{"rdonly", required_argument, 0, 'r'},
 		{"wronly", required_argument, 0, 'w'},
+		{"verbose", no_argument, 0 , 'v'},
 		{0, 0, 0, 0}
 	};
 	
@@ -37,42 +42,78 @@ int main(int argc, char *argv[])
 	
 	int fd;
 	
+	int iterator;
+	int option_index_placeholder = 0;
+	
 	while(opt != -1)
 	{
 		if(size == maxAlloc)
 		{
-			printf("I AM A FAGGOT");
+	//		printf("I AM A FAGGOT\n");
 			maxAlloc *= 2;
 			arguments = realloc(arguments, maxAlloc * sizeof(char));	
 		}			
 		
+		
+		
+		option_index_placeholder = optind - option_index_placeholder;
+	//	printf("THE OPTION INDEX IS: %d\n", optind);
+	//	printf("THE OPTION INDEX placeholder IS: %d\n", option_index_placeholder);
 		switch(opt)
 		{
+			
 			case 'r' :
+				if(verboseFlag)
+				{
+		//			printf("i hate life\n");
+
+			//		printf("THE OPTION INDEX placeholder IS: %d\n", option_index_placeholder);
+					for(iterator = 0; iterator < 2; iterator++)
+					{
+						printf("%s ", argv[option_index_placeholder + iterator]);
+					}
+					printf("\n");
+				}
 				fd = open(optarg, O_RDONLY);
 				if(fd == -1)
 				{
-					fprintf(stderr, "Error in opening file.")
+					fprintf(stderr, "Error in opening file.");
 				}
 				else
-					arguments[counter] = fd
+					arguments[counter] = fd;
 				counter++;
 				break;
 			case 'w' :
+				if(verboseFlag)
+				{
+		//			printf("i hate life\n");
+
+			//		printf("THE OPTION INDEX placeholder IS: %d\n", option_index_placeholder);
+					for(iterator = 0; iterator < 2; iterator++)
+					{
+						printf("%s ", argv[option_index_placeholder + iterator]);
+					}
+					printf("\n");
+				}
 				fd = open(optarg, O_WRONLY);
 				if(fd == -1)
 				{
-					fprintf(stderr, "Error in opening file.")
+					fprintf(stderr, "Error in opening file.");
 				}
 				else
-					arguments[counter] = fd
-				arguments[counter] = fd
+					arguments[counter] = fd;
 				counter++;
 				break;
+			case 'v' :
+				verboseFlag = 1;
+				printf("FLAG HAS BEEN SET\n");
+				break;
+				
 		}
 		size++;
+	//	printf("THE OPTION INDEX IS: %d\n", optind);
 		opt = getopt_long(argc, argv, "a", long_options, &option_index);
-		
+	//	printf("THE OPTION INDEX IS: %d\n", optind);
 	}
 	
 	exit(0);
